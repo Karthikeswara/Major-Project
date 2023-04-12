122703d3c48a3fb650672af9a7025b58b949c857
Karthik <85946586+Karthikeswara@users.noreply.github.com>
2023-04-12 13:25:34 +0530

    new code with actions

--- detection.py
+++ detection.py
@@ -1,80 +1,118 @@
 import cv2
-import mediapipe
+import mediapipe as mp
+import serial
 import time
 
-ctime=0
-ptime=0
+prevcmdval=99
 
-cap=cv2.VideoCapture(0)
 
-medhands=mediapipe.solutions.hands
-hands=medhands.Hands(max_num_hands=1,min_detection_confidence=0.7)
-draw=mediapipe.solutions.drawing_utils
+runIs = True
+print('Connecting to TTL........')
+try:
+    arduino = serial.Serial(port='COM3', baudrate=9600) # if port address changes chang the number after COM
+except:
+    print(' Failed to Connected with TTL! \n--------------------------------- \n Connect TTL with correct port\n--------------------------------- \n Windows: COM port\n--------------------------------- \n Linux or Mac dev/tty or " Google it for Mac Or Linux"')
+    print("---------------------------------\n Exiting ....")
+    runIs = False
+else:
+    # print(runIs)
+    print("successfully connected to The TTL! ")
 
-fist_shown = False
-while True:
-    success, img=cap.read()
-    img = cv2.flip(img,1)
-    imgrgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)    
-    res = hands.process(imgrgb)
+command="0"
+sendsecondscount=0    
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # Initially set finger count to 0 for each cap
+    fingerCount = 0
+
+    if results.multi_hand_landmarks:
+
+      for hand_landmarks in results.multi_hand_landmarks:
+        # Get hand index to check label (left or right)
+        handIndex = results.multi_hand_landmarks.index(hand_landmarks)
+        handLabel = results.multi_handedness[handIndex].classification[0].label
+
+        # Set variable to keep landmarks positions (x and y)
+        handLandmarks = []
+
+        # Fill list with x and y positions of each landmark
+        for landmarks in hand_landmarks.landmark:
+          handLandmarks.append([landmarks.x, landmarks.y])
+
+        # Test conditions for each finger: Count is increased if finger is 
+        #   considered raised.
+        # Thumb: TIP x position must be greater or lower than IP x position, 
+        #   deppeding on hand label.
+        if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
+          fingerCount = fingerCount+1
+        elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
+          fingerCount = fingerCount+1
+
+        # Other fingers: TIP y position must be lower than PIP y position, 
+        #   as image origin is in the upper left corner.
+        if handLandmarks[8][1] < handLandmarks[6][1]:       #Index finger
+          fingerCount = fingerCount+1
+        if handLandmarks[12][1] < handLandmarks[10][1]:     #Middle finger
+          fingerCount = fingerCount+1
+        if handLandmarks[16][1] < handLandmarks[14][1]:     #Ring finger
+          fingerCount = fingerCount+1
+        if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
+          fingerCount = fingerCount+1
+
+        # Draw hand landmarks 
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+
+    # Display finger count
+    cv2.putText(image, str(fingerCount), (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
     
-    lmlist=[]
-    tipids=[4,8,12,16,20] #list of all landmarks of the tips of fingers
-    
-    cv2.rectangle(img,(20,350),(90,440),(0,255,204),cv2.FILLED)
-    cv2.rectangle(img,(20,350),(90,440),(0,0,0),5)
-    
-    if res.multi_hand_landmarks:
-        for handlms in res.multi_hand_landmarks:
-            for id,lm in enumerate(handlms.landmark):
-                
-                h,w,c= img.shape
-                cx,cy=int(lm.x * w) , int(lm.y * h)
-                lmlist.append([id,cx,cy])
-                if len(lmlist) != 0 and len(lmlist)==21:
-                    fingerlist=[]
-                    
-                    #thumb and dealing with flipping of hands
-                    if lmlist[12][1] > lmlist[20][1]:
-                        if lmlist[tipids[0]][1] > lmlist[tipids[0]-1][1]:
-                            fingerlist.append(1)
-                        else:
-                            fingerlist.append(0)
-                    else:
-                        if lmlist[tipids[0]][1] < lmlist[tipids[0]-1][1]:
-                            fingerlist.append(1)
-                        else:
-                            fingerlist.append(0)
-                    
-                    #others
-                    for id in range (1,5):
-                        if lmlist[tipids[id]][2] < lmlist[tipids[id]-2][2]:
-                            fingerlist.append(1)
-                        else:
-                            fingerlist.append(0)
-                    
-                    
-                    if len(fingerlist)!=0:
-                        fingercount=fingerlist.count(1)
-                    
-                    
-                    cv2.putText(img,str(fingercount),(25,430),cv2.FONT_HERSHEY_PLAIN,6,(0,0,0),5)
-                    
-                #change color of points and lines
-                draw.draw_landmarks(img,handlms,medhands.HAND_CONNECTIONS,draw.DrawingSpec(color=(0,255,204),thickness=2,circle_radius=2),draw.DrawingSpec(color=(0,0,0),thickness=2,circle_radius=3))
-    
-    #fps counter
-    ctime = time.time()
-    fps=1/(ctime-ptime)
-    ptime=ctime
-    
-    #fps display
-    cv2.putText(img,f'FPS:{str(int(fps))}',(0,12),cv2.FONT_HERSHEY_PLAIN,1,(0,255,0),1)
-          
-    cv2.imshow("hand gestures",img)
-    
-    #press q to quit
-    if cv2.waitKey(1) == ord('q'):
-        break
-    
-cv2.destroyAllWindows()
+    if fingerCount > 0:
+       #if fingerCount!=prevcmdval:
+          if sendsecondscount >40:
+           
+              sendcmd=str(fingerCount)
+              print("==========SENDING COMMAND:",sendcmd)
+              arduino.write(sendcmd.encode())
+              time.sleep(2)
+              arduino.flush()
+              sendsecondscount=0
+              prevcmdval=fingerCount
+          else:
+              sendsecondscount=sendsecondscount+1
+              #print(sendsecondscount)
+
+    # Display image
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
